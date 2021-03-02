725e97242c55d10d3d6b7e53facb40a7c9076371
Ashish Gupta <ashishkuchiya@gmail.com>
2021-03-02 19:09:10 +0530

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.js'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          softviolet: 'hsl(273, 75%, 66%)',
+          softblue: 'hsl(240, 73%, 65%)',
+          darkblue: 'hsl(238, 29%, 16%)',
+          darkgray_b_v: 'hsl(237, 12%, 33%)',
+          darkgray_b: 'hsl(240, 6%, 50%)',
+          darkgray_b_l: 'hsl(240, 5%, 91%)',
+        },
+        red: {
+          softred: 'hsl(240, 5%, 91%)',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
